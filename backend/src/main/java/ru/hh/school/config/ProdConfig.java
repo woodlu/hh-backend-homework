57d8f643d83bbe98115c2f2bac3ae36b1fd314a0
woodlu <bygjly@gmail.com>
2022-04-07 14:29:01 +0300

    docker works

--- backend/src/main/java/ru/hh/school/config/ProdConfig.java
+++ backend/src/main/java/ru/hh/school/config/ProdConfig.java
@@ -1,5 +1,6 @@
 package ru.hh.school.config;
 
+import com.zaxxer.hikari.HikariConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -9,6 +10,7 @@ import ru.hh.nab.hibernate.NabHibernateProdConfig;
 import ru.hh.nab.starter.NabProdConfig;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @Import({NabHibernateProdConfig.class, NabProdConfig.class, CommonConfig.class})
@@ -16,6 +18,16 @@ public class ProdConfig {
 
   @Bean
   public DataSource dataSource(DataSourceFactory dataSourceFactory, FileSettings fileSettings) {
-    return dataSourceFactory.create("master", false, fileSettings);
+    FileSettings dataSourceSettings = fileSettings.getSubSettings("master");
+    Properties poolProperties = dataSourceSettings.getSubProperties("pool");
+    HikariConfig config = new HikariConfig(poolProperties);
+    config.setJdbcUrl("jdbc:postgresql://postgres:5432/hh");
+    config.setUsername("hh");
+    config.setPassword("hh");
+    config.setPoolName("master");
+    config.setReadOnly(false);
+    config.setValidationTimeout(config.getConnectionTimeout() + 100L);
+    return dataSourceFactory.create(config, fileSettings, false);
+//    return dataSourceFactory.create("master", false, fileSettings);
   }
 }
